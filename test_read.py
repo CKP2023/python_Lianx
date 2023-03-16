52cb33bafebdc2c2a4df60617fa636ac3a1ea109
CKP2023 <1356923362@qq.com>
2023-03-16 15:24:00 +0800

    Initial commit

---
+++ test_read.py
@@ -0,0 +1,31 @@
+# import csv
+#
+#
+path2 = "C:\\Users\\chenkp\\Desktop\\out.txt"
+with open(path2,"r") as f:
+    for read in f:
+        read = read.replace("\n","")
+        # print(read)
+
+        path1 = "out.txt"
+        with open(path1,'r') as f:
+            for txt in f:
+                reads = (txt.replace('\n',"")).split(",")
+
+                if read == reads[1]:
+                    print(reads)
+
+
+
+
+#
+#
+# path = 'C:\\Users\\chenkp\\Desktop\\out.CSV'
+# with open(path,'r') as csvfile:
+#     reader = csv.reader(csvfile)
+#     print(type(reader))
+#     for row in reader:
+#         if read == row[1]:
+#             print(type(row[1]),row[1])
+#
+# # print(read==row[1])
